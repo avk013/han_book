aa728af480ab89e5c73b448ffd619e73ec6f97a6
Kovalenko Alexander <avk013@live.ru>
2017-03-09 16:00:30 +0200

    v0

---
+++ view_terms.php
@@ -0,0 +1,96 @@
+<?php $this->load->view('general/view_header'); ?>
+<div id="content">
+	<div class="container">
+		<div class="block-content-info">
+			<div class="block-content-info-title">
+				<h1>General Terms and Conditions (GTC)</h1>
+			</div>
+			<div class="block-content-info-content fs-14 lh-20">
+<strong>1</strong>. ProFAIR (referred to as &quot;ProFAIR/we/our/us&quot;) assists in finding convenient accommodation, secures its booking/reservation, and renders relevant all-round and comprehensive services (&ldquo;Services&rdquo;) to customers (&ldquo;Guest(s)/you/your&rdquo;) according to Reservation Agreements/Invoices (&ldquo;Agreement&rdquo;) under the following basic <strong>Procedure</strong>:
+<br />
+<div class="ml-22">
+    <div style="padding: 3px 0">- You provide detailed request/order via our website or per email to our Front Office</div>
+    <div style="padding: 3px 0">- We propose corresponding option(s) of accommodation: address, specified description, location, rate, interior photos and other details upon your query</div>
+    <div style="padding: 3px 0">- After discussing details and upon your final consent, we email you a draft Reservation Agreement  combined with Invoice (with price calculation, terms of payment and other terms)</div>
+    <div style="padding: 3px 0">- You print it out, sign and seal, and then fax or email it back scanned in PDF/JPG format within 1 business day</div>
+    <div style="padding: 3px 0">- We confirm reservation and advise landlord’s contact information</div>
+    <div style="padding: 3px 0">- You make deposit down payment as per Agreement (if agreed upon)</div>
+    <div style="padding: 3px 0">- We confirm down payment receipt</div>
+    <div style="padding: 3px 0">- As soon as the guests get the itinerary, you advise arrival details and expected check-in time. We notify the landlord/-lady and arrange pick-up (if ordered) by car/minivan with our agent driver</div>
+    <div style="padding: 3px 0">- Final/Balance payment should be made as per Agreement.</div>
+</div><br />
+<strong>2</strong>. English is the only language offered for the conclusion of the Agreement.
+<br /><br />
+<strong>3</strong>. ProFAIR only acts as an agent finding and booking accommodation for the Guests and it takes no responsibility for accuracy of the information received from the landlord/host of the object to rent. However, the information can be verified and refined if needed upon the Guests&rsquo; query.
+<br /><br />
+<strong>4</strong>. Additional terms demanded by the landlord (e.g. refundable security deposit) shall be mentioned in the Reservation Agreement but not included in its Total price.
+<br /><br />
+<strong>5</strong>. Reservation <strong>Agreement guarantees accommodation for Guests and guarantees their implicit payment to ProFAIR</strong>. When making a reservation of the property chosen, the Guests accept the Agreement in the whole. In case of force majeure, the Guests should be provided with an equivalent accommodation.
+<br /><br />
+<strong>6</strong>. Reservation Agreement comes in force after receipt by ProFAIR the Agreement duly signed and sealed by the Guests&#39; Company head/executive (if business trip) or by the Guest if private.<br/>Duly paid Agreement-Invoice or duly paid single Invoice comes in force as well, and it is considered duly signed with our GTC accepted in the whole.
+<br /><br />
+<strong>7</strong>. Cancellation through the Guests.
+<br />
+<div class="ml-22">
+    <div style="padding: 3px 0">7.1 For the cancellation, following cancellation costs are put bottom:</div>
+    <div style="padding: 3px 22px">- up to7 days before arrival start of 80% the calculation Total</div>
+    <div style="padding: 3px 22px">- 8-14 days before arrival start of 50% the calculation Total</div>
+    <div style="padding: 3px 22px">- 15 days-1 month before arrival start of 30% the calculation Total</div>
+    <div style="padding: 3px 22px">- more than 1 month before arrival start of 15% the calculation Total</div>
+    <div style="padding: 3px 0">7.2 Cancellation must be explained in writing at least a week before the planned check-in. Decisive for the cancellation point the entrance of the explanation is here.</div>
+</div>
+<br />
+<h4><strong>8</strong>. House Regulations</h4>
+<div class="ml-22">
+      8.1. The Guests must observe the rules of so called "Hausordnung", e.g.: no loud sounds/noises made or litter dropped in the house<br/> and in front of it.
+</div><br />
+<div class="ml-22">
+      8.2. If Internet connection provided by the host (LAN / WLAN), the Guests have no right to use any paid websites (e.g. to download content, etc.).   
+      Otherwise, the Guests are solely liable for all costs which incurred in this regard.»
+</div>
+<br />
+<h4><strong>9</strong>. Damaging</h4>
+<div class="ml-22">
+In case of any kind of loss/damaging of the rooms / flat / house/accessories/belongings or furniture in these objects made by the Guests, the latter are liable for all costs which incurred in this regard (immediate reimbursement in cash).»
+</div>
+<br /><br />
+<h4><strong>10</strong>. Terms of payment</h4>
+<div class="ml-22">
+    <div style="padding: 3px 0">10.1. Payment for the Services shall be effected by the Guests to ProFAIR strictly in accordance with the Agreement concluded. Unless specially stated in the Agreement, payment methods possible/accepted by ProFAIR are specified on our website. Some payment terms may be updated and refined in the Agreement.</div>
+    <div style="padding: 3px 0">10.2. Payment by a Third Party for and on behalf of the Guests and payment by instalments according to agreed schedule are possible.</div>
+    <div style="padding: 3px 0">10.3. Schedule for payment by instalments shall be correlated with corresponding cancellation terms, i.e. payment received should be sufficient at any time to cover actual cancellation charge/fee in case the Reservation Agreement is cancelled.</div>
+    <div style="padding: 3px 0">10.4. To secure acceptance of the Guests’ online payments by bank cards and processing of card transactions, ProFAIR co-operates with AS PrivatBank, Riga, as acquiring bank (www.privatbank.lv/en/) and its web interface LiqPay. We accept VISA/MasterCard/Maestro card. The procedure of payment is briefly described in the Agreement.</div>
+    <div style="padding: 3px 0">10.5. LiqPay system ensures 24/7 secure online card payment through the bank protected server under PCI DSS security standard and 3D-Secure technology. ProFAIR and LiqPay never store the Payer’s card information (card number, etc.).</div>
+    <div style="padding: 3px 0">10.6. Mistakenly received payments shall be refunded by ProFAIR to the Guests/payers in a manner and time agreed upon.</div>
+    <div style="padding: 3px 0">10.7. In case of breach of payment terms by the Guests, they/their Company undertake(s) to unconditionally reimburse corresponding amount and/or cancellation costs as per above GTC.7. Otherwise, the guests lose the right to accommodation and should move out.</div>
+    <div style="padding: 3px 0">
+        10.8. Accommodation price quoted by the landlord in the separate Arrangement/agreement with ProFAIR, shall be paid to the landlord
+        <div style="padding-left: 20px;">a) by ProFAIR (if included in the Total price of Reservation Agreement) or, otherwise,</div>
+        <div style="padding-left: 20px;">b) by the Guest when check-in (according to the Reservation Agreement).</div>
+    </div>
+</div>
+<br />
+<h4><strong>11</strong>. Privacy</h4>
+<div class="ml-22">
+    and Security Protecting our Customer&#39;s personal information is extremely important to us. The information is kept physically secure and we never share this information with any Third Party. <strong>Neither we nor our acquiring bank store your bank cards data. </strong>
+</div>
+<br />
+<h4><strong>12</strong>. Miscellaneous</h4>
+<div class="ml-22">
+    <div style="padding: 3px 0">12.1. The Agreement is considered to be fulfilled as soon as both the payment is received in full by ProFAIR and the Guests have checked-in without any written complaints/claims.</div>
+    <div style="padding: 3px 0">12.2. In case of no-show or unjustified refusal to check-in, the Guests lose the right to accommodation and the Agreement is considered to be fulfilled by ProFAIR and terminated.</div>
+    <div style="padding: 3px 0">12.3. We reserve the right to change these GTC at any time. The changes will take effect when posted on our website.</div>
+</div><br />
+<h4><strong>13</strong>. Governing law, jurisdiction and dispute resolution</h4>
+<div class="ml-22">
+Any claim or dispute arising from or relating to the Reservation Agreement (including non-contractual disputes or claims and their interpretation) shall be resolved/settled amicably through negotiations. If settlement is not reached, the dispute in question shall be subject to the non-exclusive jurisdiction of the English courts (submitted for arbitration in England at the London Court of Arbitration and according to English law). The findings of the Court of Arbitration shall be considered as final, irrevocable and binding. At all times, English language shall prevail.
+</div>
+<!-- <br /><br />
+Arrangements/agreements between ProFAIR and landlords (including non-contractual disputes or claims and their interpretation) shall be governed by the laws of Germany. Any claim or dispute arising from or relating to the Arrangements should be by default solely resolved/settled amicably through negotiations. -->
+
+
+
+        </div>
+	</div>
+</div>
+<?php $this->load->view('general/view_footer'); ?>
